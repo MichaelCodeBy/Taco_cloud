3d00e7d0c48546f3d9810c9bd174c15a9bd712e9
Michael <gomellc@gmail.com>
2022-12-29 14:50:16 +0300

    find and remove errors from test

--- src/test/java/HomeControllerTest.java
+++ src/test/java/HomeControllerTest.java
@@ -3,6 +3,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static java.nio.file.Paths.get;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
@@ -12,9 +20,9 @@ public class HomeControllerTest {
 
     @Test
     public void testHomePage() throws Exception{
-        mockMvc.perform(get("/"))
+        mockMvc.perform((RequestBuilder) get("/"))
                 .andExpect(status().isOk())
-                .andExpect(viev().name("home"))
-                .andExpect(content().string(containsString("Welcome to ...")));
+                .andExpect(view().name("home"))
+                .andExpect((ResultMatcher) content().string(containsString("Welcome to ...")));
     }
 }
